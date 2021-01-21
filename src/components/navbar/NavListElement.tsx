a419031bb28b91f559d2e2fd4d87af55f9fe85d4
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-20 23:47:49 -0700

    finished a basic navbar

--- src/components/navbar/NavListElement.tsx
+++ src/components/navbar/NavListElement.tsx
@@ -2,13 +2,33 @@ import styled from "styled-components";
 
 const NavListElement = styled.li`
 	width: 100%;
+	font-weight: bold;
+	font-size: 1.2rem;
 
 	:first-child {
 		text-transform: uppercase;
+		font-size: 2rem;
+		overflow: hidden;
+		white-space: nowrap;
+		margin-bottom: 1rem;
+		text-align: center;
+		letter-spacing: 0.02ch;
+		width: 100%;
+	}
+
+	:first-child img {
+		min-width: 3rem;
+		max-width: 3rem;
+		margin: 0 1rem;
 	}
 
 	:last-child {
 		margin-top: auto;
+	}
+
+	:hover {
+		background-color: var(--nord4);
+		color: var(--nord1);
 	}
 `;
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,6 +6,9 @@ import NavLink from "components/navbar/NavLink";
 import NavListImg from "components/navbar/NavListImg";
 import NavLinkText from "components/navbar/NavLinkText";
 import crow from "images/crow.svg";
+import feather from "images/feather.svg";
+import code from "images/code.svg";
+import user from "images/user.svg";
 
 interface Props {}
 
@@ -13,10 +16,10 @@ const NavBar = styled.nav`
 	width: 5rem;
 	height: 100vh;
 	position: fixed;
-	transition: width 300ms ease;
+	transition: width 300ms ease-out;
 
 	:hover {
-		width: 16rem;
+		width: 20rem;
 	}
 
 	:hover span {
@@ -27,20 +30,29 @@ const NavBar = styled.nav`
 interface navbarElement {
 	img: any;
 	text: string;
+	src: string;
 }
 
 const navbarElements: navbarElement[] = [
 	{
 		img: crow,
-		text: "hello",
+		text: "Kyle McKell",
+		src: "about",
 	},
 	{
-		img: "crow",
-		text: "hello",
+		img: user,
+		text: "About",
+		src: "about",
 	},
 	{
-		img: "crow",
-		text: "hello",
+		img: code,
+		text: "Portfolio",
+		src: "portfolio",
+	},
+	{
+		img: feather,
+		text: "Contact",
+		src: "contact",
 	},
 ];
 
@@ -58,6 +70,7 @@ export const Navbar = (props: Props) => {
 						</NavListElement>
 					);
 				})}
+				<NavListElement>Hi</NavListElement>
 			</NavBarNav>
 		</NavBar>
 	);
