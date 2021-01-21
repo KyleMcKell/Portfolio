047aa79ba6a6e1a43f4847a8ddef9ab3a2400d09
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-20 23:14:34 -0700

    figured out the map needed a return value, fixed that and now the navbar works

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import NavBarNav from "components/navbar/NavbarNav";
 import NavLink from "components/navbar/NavLink";
 import NavListImg from "components/navbar/NavListImg";
 import NavLinkText from "components/navbar/NavLinkText";
+import crow from "images/crow.svg";
 
 interface Props {}
 
@@ -24,13 +25,13 @@ const NavBar = styled.nav`
 `;
 
 interface navbarElement {
-	img: string;
+	img: any;
 	text: string;
 }
 
 const navbarElements: navbarElement[] = [
 	{
-		img: "crow",
+		img: crow,
 		text: "hello",
 	},
 	{
@@ -48,13 +49,14 @@ export const Navbar = (props: Props) => {
 		<NavBar>
 			<NavBarNav>
 				{navbarElements.map((el) => {
-					<NavListElement>
-						<NavLink>
-							{el.text}
-							<NavListImg src={`images/${el.img}.svg`} alt={el.img} />
-							<NavLinkText></NavLinkText>
-						</NavLink>
-					</NavListElement>;
+					return (
+						<NavListElement>
+							<NavLink>
+								<NavListImg src={`${el.img}`} alt={"ah shoot"} />
+								<NavLinkText>{el.text}</NavLinkText>
+							</NavLink>
+						</NavListElement>
+					);
 				})}
 			</NavBarNav>
 		</NavBar>
