27064ada7519e64cbd98793a9e934f6ba41de2c0
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-01-23 21:18:53 -0700

    added a picture of myself

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import LogoContainer from "components/LogoContainer";
-import css3 from "images/logos/css3.png";
-import git from "images/logos/git.png";
-import html5 from "images/logos/html5.png";
-import js from "images/logos/javascript.png";
-import react from "images/logos/react.png";
-import ts from "images/logos/typescript.png";
+import css3 from "images/tech-icons/css3.png";
+import git from "images/tech-icons/git.png";
+import html5 from "images/tech-icons/html5.png";
+import js from "images/tech-icons/javascript.png";
+import react from "images/tech-icons/react.png";
+import ts from "images/tech-icons/typescript.png";
 import TechnologyLogo from "components/TechnologyLogo";
 import H2Text from "components/H2Text";
 import Separator from "components/Separator";
+import selfie from "images/me.jpg";
+import styled from "styled-components";
 
 interface Props {}
 
-interface logo {
+interface TechIcon {
 	src: any;
 	name: string;
 }
 
-const logos: logo[] = [
+const techIcons: TechIcon[] = [
 	{
 		src: html5,
 		name: "html5",
@@ -44,15 +46,42 @@ const logos: logo[] = [
 	},
 ];
 
+const AboutSelfDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+`;
+
+const TextDiv = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: space-around;
+	align-items: center;
+`;
+
+const SelfieStyle = styled.img`
+	height: 15rem;
+	width: 15rem;
+	border-radius: 50%;
+	border: 5px solid var(--nord9);
+	filter: grayscale(50%);
+`;
+
 export const About = (props: Props) => {
 	return (
 		<>
-			<H2Text>Hey there 🤙</H2Text>
-			<H2Text> I'm a React Front-End Developer</H2Text>
+			<AboutSelfDiv>
+				<TextDiv>
+					<H2Text>Hey there 🤙</H2Text>
+					<H2Text>I'm Kyle</H2Text>
+					<H2Text>I'm a Front-End React Developer</H2Text>
+				</TextDiv>
+				<SelfieStyle src={selfie} alt="picture of me" />
+			</AboutSelfDiv>
 			<Separator />
 			<LogoContainer>
-				{logos.map((logo) => {
-					return <TechnologyLogo src={logo.src} alt={logo.name} />;
+				{techIcons.map((icon) => {
+					return <TechnologyLogo src={icon.src} alt={icon.name} />;
 				})}
 			</LogoContainer>
 		</>
